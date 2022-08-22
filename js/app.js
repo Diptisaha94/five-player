575a09a9b35b3810fe46aad040db9da891e8e204
Dipti saha <sahadipti322@gmail.com>
2022-08-22 10:07:21 -0400

    budget part and image add

--- js/app.js
+++ js/app.js
@@ -17,5 +17,29 @@ function selectedPlayer(element){
         alert("you are already five player selected");
     }
    displayPlayer(playerArray);
-   
+document.getElementById('selected-player').innerText = playerArray.length;  
 }
+function getElementbyValue(element){
+    const playerElement = document.getElementById(element);
+const playerString = playerElement.value;
+const playerNumber = parseFloat(playerString);
+return playerNumber;
+}
+
+document.getElementById("calculate-btn").addEventListener('click',function(){
+    const perPlayerCost = getElementbyValue("per-player");
+    const playerExpenses = document.getElementById("player-expenses");
+    const totalCost = perPlayerCost * playerArray.length;
+    playerExpenses.innerText = totalCost;
+})
+document.getElementById("calc-total-btn").addEventListener('click',function(){
+    const perPlayerCost = getElementbyValue("per-player");
+    const playerExpenses = document.getElementById("player-expenses");
+    const totalCost = perPlayerCost * playerArray.length;
+    playerExpenses.innerText = totalCost;
+    const managerCost = getElementbyValue("manager");
+    const coachCost = getElementbyValue("coach");
+    const totalPlayerCost = totalCost + managerCost + coachCost;
+    const totalElement = document.getElementById("total");
+    totalElement.innerText = totalPlayerCost;
+})
